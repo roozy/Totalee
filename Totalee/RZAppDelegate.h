a32b2a83a434f326b36c231030a42669ab86440c
Andy Roth <rothacr@gmail.com>
2012-06-15 13:20:47 -0700

    Initial commit

---
+++ Totalee/RZAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  RZAppDelegate.h
+//  Totalee
+//
+//  Created by Andy Roth on 6/15/12.
+//  Copyright (c) 2012 Roozy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Totalee/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Totalee
+//
+//  Created by Andy Roth on 6/15/12.
+//  Copyright (c) 2012 Roozy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RZAppDelegate class]));
+    }
+}
